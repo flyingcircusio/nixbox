a2388a3f3c1fc86f2195189dda62f7ff17f8019c
Christian Zagrodnick <cz@flyingcircus.io>
2017-11-16 07:53:38 +0100

    add build script and automatically find out channel version

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+export NIXOS_BUILD=$(curl -s --head https://hydra.flyingcircus.io/channels/branches/fc-15.09-production/ | \
+    grep Location | \
+    sed -E 's/.*15.09\.([0-9]+)\..*/\1/')
+
+
+echo Building v$NIXOS_BUILD
+
+exec packer build nixos-x86_64.json
